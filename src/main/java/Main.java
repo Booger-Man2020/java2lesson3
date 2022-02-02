a580979d44deba149a14f0f2456d1187f4b5a55c
Aleh <vatsevicho@gmail.com>
2022-02-02 18:42:17 +0300

    Дз сделано

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.stream.Collectors;
+
 public class Main {
 
 
     public static void main(String[] args) {
-//       firstTask();
+       firstTask();
 
         PhoneBook phoneBook =new PhoneBook();
         phoneBook.add("Петров", String.valueOf(375291565));
@@ -14,37 +18,39 @@ public class Main {
         phoneBook.seach("Петров");
 
     }
-    
-//    public static void firstTask(){
-//        ArrayList<String> list = new ArrayList<>();
-//        list.add("Рыба");
-//        list.add("Мясо");
-//        list.add("Курица");
-//        list.add("Яйцо");
-//        list.add("Масло");
-//        list.add("Мясо");
-//        list.add("Мука");
-//        list.add("Вода");
-//        list.add("Рыба");
-//        list.add("Рыба");
-//        list.add("Курица");
-//        list.add("Баранина");
-//        list.add("Макароны");
-//        list.add("Салат");
-//        list.add("Яйцо");
-//        list.add("Макароны");
-//        ArrayList<String> list1 = new ArrayList<>();
-//        list1 = (ArrayList<String>) list.stream().distinct().collect(Collectors.toList());
-//
-//        for (int i = 0; i < list1.size(); i++) {
-//            System.out.println(list1.get(i));
-//        }
-//
-//        int count = Collections.frequency(list, "Рыба");
-//        System.out.println("Рыба " + count + " раз(а)");
-//        count = Collections.frequency(list, "Мясо");
-//        System.out.println("Мясо " + count + " (а)");
-//    }
+
+    public static void firstTask(){
+        ArrayList<String> list = new ArrayList<>();
+        list.add("Рыба");
+        list.add("Мясо");
+        list.add("Курица");
+        list.add("Яйцо");
+        list.add("Масло");
+        list.add("Мясо");
+        list.add("Мука");
+        list.add("Вода");
+        list.add("Рыба");
+        list.add("Рыба");
+        list.add("Курица");
+        list.add("Баранина");
+        list.add("Макароны");
+        list.add("Салат");
+        list.add("Яйцо");
+        list.add("Макароны");
+        ArrayList<String> list1;
+        list1 = (ArrayList<String>) list.stream().distinct().collect(Collectors.toList());
+
+        for (int i = 0; i < list1.size(); i++) {
+            System.out.println(list1.get(i));
+        }
+
+        int count = Collections.frequency(list, "Рыба");
+        System.out.println("Рыба " + count + " раз(а)");
+        count = Collections.frequency(list, "Мясо");
+        System.out.println("Мясо " + count + " раз(а)");
+        count = Collections.frequency(list, "Курица");
+        System.out.println("Курица " + count + " раз(а)");
+    }
 
 }
 
